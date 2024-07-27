fe0cf02346db95aec01bc5d2c656e11144808af5
Yoshimi Araki <yoshimi@Yoshimis-Laptop.local>
2024-07-27 13:03:30 -0400

    fav partially working

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -5,9 +5,17 @@ const router = express.Router();
 
 // GET Requests to retrieve data using the middleware routes
 
-router.get('/test_get_data', SB_controller.get_data_test, (req, res) => {
+
+router.post('/getFav', SB_controller.get_fav, (req, res) => {
   return res.status(200);
 })
+
+router.post('/addFav', SB_controller.add_fav, (req, res) => {
+  return res.status(200);
+})
+
+
+
 
 router.get('/coins', tiApiController.coinListMiddleware, (req, res) => {
   return res.status(200).json(res.locals);
@@ -24,6 +32,33 @@ router.get('/completeCoin/:id', tiApiController.completeCoinMiddleware, (req, re
 router.get('/historyCoin/:id', tiApiController.historyCoinMiddleware, (req, res) => { // Updated to use dynamic middleware
   return res.status(200).json(res.locals);
 });
+
+
+// router.get("/auth/callback", async function (req, res) {
+//   const code = req.query.code
+//   const next = req.query.next ?? "/"
+
+//   if (code) {
+//     const supabase = createServerClient(
+//       process.env.SUPABASE_URL,
+//       // replaced SUPABASE_ANON_KEY to SUPABASE_KEY
+//       process.env.SUPABASE_KEY, {
+//     cookies: {
+//       getAll() {
+//         return parseCookieHeader(context.req.headers.cookie ?? '')
+//       },
+//       setAll(cookiesToSet) {
+//         cookiesToSet.forEach(({ name, value, options }) =>
+//           context.res.appendHeader('Set-Cookie', serializeCookieHeader(name, value, options))
+//         )
+//       },
+//     },
+//   })
+//     await supabase.auth.exchangeCodeForSession(code)
+//   }
+
+//   res.redirect(303, `/${next.slice(1)}`)
+// })
 
 
 module.exports = router;
